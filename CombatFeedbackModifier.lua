bf0f30c733655dd6f5a0e6a43a80fd23f8e777e8
Andrea Zerbini <zerbini.andrea.97@gmail.com>
2025-01-04 17:34:08 +0100

    feat: add addon files

---
+++ CombatFeedbackModifier.lua
@@ -0,0 +1,7 @@
+-- Set the combat feedback fade in, hold, and fade out times
+COMBATFEEDBACK_FADEINTIME = 0
+COMBATFEEDBACK_HOLDTIME = 0
+COMBATFEEDBACK_FADEOUTTIME = 0
+
+-- Print confirmation to the chat that the addon loaded
+DEFAULT_CHAT_FRAME:AddMessage("Combat Feedback Modifier loaded. Fade times set to 0.")
